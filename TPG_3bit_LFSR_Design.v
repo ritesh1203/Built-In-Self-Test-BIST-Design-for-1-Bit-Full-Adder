77b22e23e86ab10475107c1d80122672b1f95052
Ritesh Kumar <rk1516924@gmail.com>
2025-11-16 00:38:36 +0530

    TPG_3bit_LFSR_Design.v

--- TPG_3bit_LFSR_Design.v
+++ TPG_3bit_LFSR_Design.v
@@ -1,50 +1,28 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.09.2025 18:25:07
-// Design Name: 
-// Module Name: TPG_3bit_LFSR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns / 1ps
-module TPG_3bit_LFSR(data_out, complete, reset, clock);
-	output [2:0] data_out;
-    output reg complete;
-    input reset;
-    input clock;
-    reg [0:2] lfsr_reg;
+module TPG_3bit_LFSR (data_out, complete, reset, clock);
+    output  reg  [2:0] data_out;
+    output reg  complete;
+    input  reset, clock;
     reg [2:0] counter;
-
-    always @(posedge clock or posedge reset) begin
-        if (reset == 1) begin
-            lfsr_reg <= 3'b001;  // seed value = 1
+    always  @(posedge clock or negedge reset)  begin
+        if (!reset) begin
+            data_out  <= 3'b001;    // seed value = 001
             counter  <= 3'b000;
-            complete <= 0;
+            complete  <= 0;
         end
-        else begin
-            // Polynomial: x^3 + x + 1
-            lfsr_reg[0] <= lfsr_reg[2];
-			lfsr_reg[1] <= lfsr_reg[0] ^ lfsr_reg[2];
-			lfsr_reg[2] <= lfsr_reg[1];
-            if (counter == 3'b110)  // after 7 unique states
-                complete <= 0;
+        else  begin       // Polynomial: x^3 + x + 1
+            data_out[1]  <=  data_out [3];
+            data_out[2]  <=  data_out [1]  ^  data_out [3];
+            data_out[3]  <=  data_out [2];
+            counter  <=  counter + 1;
+            if (counter == 3’b110)    // after 7 unique states(0 - 6)
+                   complete  <=  1;
             else
-            	complete <= 1;
-            end
-        end
-    assign data_out = lfsr_reg;
+            	   complete  <=  0;
+         end
+      end
 endmodule
+
 
 
 
