9962e4d2de836a28921e304e00f2cc203508e24e
Ritesh Kumar <rk1516924@gmail.com>
2025-11-16 00:49:13 +0530

    TPG_3bit_LFSR_Testbench.v

--- TPG_3bit_LFSR_Testbench.v
+++ TPG_3bit_LFSR_Testbench.v
@@ -1,39 +1,17 @@
 `timescale 1ns / 1ps
-
 module TPG_3bit_LFSR_tb;
     reg clock, reset;
     wire [2:0] data_out;
     wire complete;
-
-    // Instantiate DUT
-    TPG_3bit_LFSR DUT (
-        .data_out(data_out),
-        .complete(complete),
-        .reset(reset),
-        .clock(clock)
-    );
-
-    // Clock generation (10ns period)
+    TPG_3bit_LFSR DUT (.data_out(data_out), .complete(complete), .reset(reset), .clock(clock));   // Instantiate DUT
     initial begin
         clock = 1'b0;
-    end
-    always #5 clock = ~clock;
-
-    // Reset sequence
-    initial begin                                                                                              
-        reset = 1'b1;       // assert reset
-        #10 reset = 1'b0;   // release reset
-    end
-                                                                                                                                                                                                                                        
-    // Monitor signals + VCD dump                                                                                                                                              
-    initial begin
-        $dumpfile("TPG_3bit_LFSR.vcd");
+        reset = 1'b0;                  // Reset sequence
+        always #5 clock = ~clock;      // Clock generation (10ns period                                                                         
+        #10 reset = 1'b1;              // release reset                                                                                                                                                                                                                                                                                                                                                                                                            
+        $dumpfile("TPG_3bit_LFSR.vcd"); // Monitor signals + VCD dump 
         $dumpvars(0, TPG_3bit_LFSR_tb);
-        $monitor($time, " data_out = %b , complete = %b, reset = %b, clock = %b", data_out                                                                                                                 , complete, reset, clock);
-    end
-
-    // Finish simulation
-    initial begin
-        #200 $finish;
+        $monitor($time, " data_out = %b , complete = %b, reset = %b, clock = %b", data_out, complete, reset, clock);                                                                                                                 , complete, reset, clock);
+        #200 $finish;                   // Finish simulation
     end
 endmodule
