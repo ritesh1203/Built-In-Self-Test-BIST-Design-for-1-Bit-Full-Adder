5a0ba8582a14add83c595166ac68145284232bf2
Ritesh Kumar <rk1516924@gmail.com>
2025-11-16 23:19:46 +0530

    CUT_1bit_full_adder_Design.v

--- CUT_1bit_full_adder_Design.v
+++ CUT_1bit_full_adder_Design.v
@@ -1,22 +1,16 @@
 `timescale 1ns / 1ps
 module CUT_1bit_full_adder(a, b, cin, f1, f2, f3, sum, cout);
-	input   a, b, cin;
-    input   f1;   // fault injection for a
-    input   f2;   // fault injection for r
-    input   f3;   // fault injection for sum
+	input   a, b, cin, f1, f2, f3;                                 
     output  sum, cout;
     wire  a1, p, sum1, r, r1, q;
-    assign a1 = a & f1;           // Fault injection on 'a' (a stuck-at-0 when f1=0)
-    // XOR stage for sum calculation
-    assign p    = a1 ^ b;       // first XOR
-    assign sum1 = p ^ cin;      // second XOR
-    // Fault injection on sum (sum stuck-at-1 when f3=1)
-    assign sum = sum1 | f3;
+	// sum calculation
+	assign a1 = a & f1;          // fault injection on a 
+    assign p  = a1 ^ b;       
+    assign sum1 = p ^ cin;      
+    assign sum = sum1 | f3;     // Fault injection on sum
     // Carry calculation
-    assign r  = a1 & b;         // AND gate
-    assign r1 = r & f2;         // fault injected r
-    assign q  = p & cin;        // AND gate for carry
-    assign cout = q | r1;       // OR gate for final carry
+    assign r  = a1 & b;         
+    assign r1 = r & f2;         // fault injected on r
+    assign q  = p & cin;        
+    assign cout = q | r1;       
 endmodule
-
-
