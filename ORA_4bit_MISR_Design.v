2e8f45e0bd154a1f61741ab5d1d3da4d484b41e1
Ritesh Kumar <rk1516924@gmail.com>
2025-11-17 00:05:23 +0530

    ORA_4bit_MISR_Design.v

--- ORA_4bit_MISR_Design.v
+++ ORA_4bit_MISR_Design.v
@@ -1,37 +1,17 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.09.2025 19:36:46
-// Design Name: 
-// Module Name: ORA_4bit_MISR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns / 1ps
-module ORA_4bit_MISR(dataIn, reset, clock, dataOut);
+module ORA_4bit_MISR (dataIn, clock, reset, dataOut);
 	input [1:0] dataIn;        // dataIn[0] = Sum, dataIn[1] = Cout
-	input reset,clock;
-	output reg [3:0] dataOut;  // {Q3,Q2,Q1,Q0}
-	always@(posedge clock or posedge reset)
-	begin
-		if(reset == 1)
-			dataOut <= 4'b0000; // Reset state
-		else
-			begin
-				dataOut[0] <= dataOut[3] ^ dataIn[0];              // feedback + dataIn[0]
-				dataOut[1] <= dataOut[3] ^ dataOut[0] ^ dataIn[1]; // feedback + dataIn[1]
-				dataOut[2] <= dataOut[1] ;                         // shift
-				dataOut[3] <= dataOut[2] ;                         // shift
+	input clock, reset;
+	output reg [3:0] dataOut;
+	always @(posedge clock or negedge reset) begin
+		if(!reset) begin
+			dataOut <= 4'b0000; 
+		end
+		else begin
+			dataOut[1] <= dataOut[3] ^ dataIn[0];              // feedback + dataIn[0]
+			dataOut[2] <= dataOut[3] ^ dataOut[0] ^ dataIn[1]; // feedback + dataIn[1]
+			dataOut[3] <= dataOut[1] ;                         // shift
+			dataOut[4] <= dataOut[2] ;                         // shift
 			end
 	end
 endmodule
