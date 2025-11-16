f9b176437e45c2f9cfda52334cc376334256118d
Ritesh Kumar <rk1516924@gmail.com>
2025-11-17 00:25:59 +0530

    ORA_4bit_MISR_Testbench.v

--- ORA_4bit_MISR_Testbench.v
+++ ORA_4bit_MISR_Testbench.v
@@ -1,42 +1,17 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// Create Date: 05.09.2025 20:04:48
-// Design Name: 
-// Module Name: ORA_4bit_MISR_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// Dependencies: 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 module ORA_4bit_MISR_tb;
+    reg [1:0] dataIn;
     reg clock, reset;
-    reg [1:0] dataIn;    // {Cout, Sum}
-    wire [3:0] dataOut;  // {Q3,Q2,Q1,Q0}
-    // Instantiate the DUT (Device Under Test)
-    ORA_4bit_MISR DUT (.dataIn(dataIn), .reset(reset), .clock(clock), .dataOut(dataOut));
-    // Clock generation: 10ns period
-    // Clock generation: 10ns period
-    initial clock = 0;
-    always #5 clock = ~clock;
-    initial
-    	begin
-    		$dumpfile("ORA_4bit_MISR.vcd");
-    		$dumpvars(0, ORA_4bit_MISR_tb);
-    		$monitor($time, "dataIn = %b, reset = %b, clock = %b, dataOut = %b", dataIn, reset, clock, dataOut);
-    	end
-    // Stimulus
+    ORA_4bit_MISR  DUT (.dataIn(dataIn), .clock(clock), .reset(reset), .dataOut(dataOut));    // Instantiate the DUT (Device Under Test)
     initial begin
-        dataIn = 2'b00;   // Initialize inputs (Cout=0, Sum=0)
-        reset  = 1'b1;       // Apply reset
-        #10 reset = 1'b0;    // Hold reset a bit longer, then release
-
+        clock = 1'b0;
+        reset  = 1'b0;
+        always #5 clock = ~clock;
+    	$dumpfile ("ORA_4bit_MISR.vcd");
+    	$dumpvars (0, ORA_4bit_MISR_tb);
+    	$monitor ($time, "dataIn = %b, reset = %b, clock = %b, dataOut = %b", dataIn, reset, clock, dataOut);
+        dataIn = 2'b00;       // Initialize inputs (Sum=0, Cout=0)
+        #10 reset = 1'b1;    // Hold reset a bit longer, then release
         // Apply test patterns (Sum,Cout)
         #10 dataIn = 2'b10;   // Sum=0, Cout=1
         #10 dataIn = 2'b01;   // Sum=1, Cout=0
@@ -45,8 +20,7 @@ module ORA_4bit_MISR_tb;
         #10 dataIn = 2'b01;   // Sum=1, Cout=0
         #10 dataIn = 2'b10;   // Sum=1, Cout=0
         #10 dataIn = 2'b10;   // Sum=0, Cout=1
-
-        #20 $finish;
+        #200 $finish;
     end
 endmodule
 
