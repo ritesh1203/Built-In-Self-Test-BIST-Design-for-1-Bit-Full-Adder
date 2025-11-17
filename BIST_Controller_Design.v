70a5fa5351f2ae7bf5c652d006908a6f0f8f2e60
Ritesh Kumar <rk1516924@gmail.com>
2025-11-17 22:06:35 +0530

    BIST_Controller_Design.v

--- BIST_Controller_Design.v
+++ BIST_Controller_Design.v
@@ -9,20 +9,13 @@ module BIST_Controller (clock, reset, testmode, w_x_y, data_out, dataIn, dataOut
     wire a, b, cin;
     wire complete;
     wire sum, cout;
-    // Expected golden signature
-    parameter golden_signature = 4'b0011;
-    // Input selection (Normal vs BIST mode)
-    assign {a, b, cin} = testmode ? data_out[2:0] : w_x_y;
-    // Group full adder outputs into MISR input
-    assign dataIn = {sum, cout};
-    // LFSR (Test Pattern Generator)
-    TPG_3bit_LFSR 	M1 (.data_out(data_out), .complete(complete), .reset(reset), .clock(clock));
-    // CUT (1-bit Full Adder with fault injection disabled)
-    CUT_1bit_full_adder M2 (.a(a), .b(b), .cin(cin), .f1(1'b1), .f2(1'b1), .f3(1'b0), .sum(sum), .cout(cout));
-    // MISR (Output Response Analyzer)
-    ORA_4bit_MISR M3 (.dataIn(dataIn), .reset(reset), .clock(clock), .dataOut(dataOut));
-    // Fault detection logic
-    always @(posedge clock) begin
+    parameter golden_signature = 4'b0011;  // Expected golden signature
+    assign {a, b, cin} = testmode ? data_out[2:0] : w_x_y; // Input selection (Normal vs BIST mode)
+    assign dataIn = {sum, cout};  // Group full adder outputs into MISR input
+    TPG_3bit_LFSR 	M1 (.clock(clock), .reset(reset), .data_out(data_out), .complete(complete));   // LFSR (Test Pattern Generator)
+    CUT_1bit_full_adder M2 (.a(a), .b(b), .cin(cin), .f1(1'b1), .f2(1'b1), .f3(1'b0), .sum(sum), .cout(cout)); // CUT (1-bit Full Adder with fault injection disabled)
+    ORA_4bit_MISR M3 (.dataIn(dataIn), .clock(clock), .reset(reset), .dataOut(dataOut));  // MISR (Output Response Analyzer)
+    always @(posedge clock) begin      // Fault detection logic
         if (testmode == 0) begin
             fault_detected <= 0;
         end 
