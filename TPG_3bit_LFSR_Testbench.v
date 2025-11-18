042ce56c92336fd1fc6e35d89d2a99a313cbd2ff
Ritesh Kumar <rk1516924@gmail.com>
2025-11-18 18:08:34 +0530

    TPG_3bit_LFSR_Testbench.v

--- TPG_3bit_LFSR_Testbench.v
+++ TPG_3bit_LFSR_Testbench.v
@@ -6,11 +6,11 @@ module TPG_3bit_LFSR_tb;
     TPG_3bit_LFSR DUT(.clock(clock), .reset(reset), .dataout_tpg(dataout_tpg), .complete(complete));   // Instantiate DUT
     initial begin
     	reset = 1'b0;
-    	#10 reset = 1'b1;                  // release reset     
+    	#10 reset = 1'b1;                 // release reset  
         clock = 1'b0;                 
         forever #5 clock = ~ clock;      // Clock generation (10ns period)                                                                        
     end
-    initial begin                                                                                                                                                                                                                                                                                                                                                                                                                 
+    initial begin                                                                                                                                                                                                                                                                                                                                                                                                                              
         $dumpfile("TPG_3bit_LFSR.vcd"); // Monitor signals + VCD dump 
         $dumpvars(0, TPG_3bit_LFSR_tb);
         $monitor($time, "clock = %b, reset = %b, dataout_tpg = %b, complete = %b", clock, reset, dataout_tpg, complete);                                                                                                                
