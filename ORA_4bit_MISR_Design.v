6f88e674098fb646a78e5d953493aed9e642dd47
Ritesh Kumar <rk1516924@gmail.com>
2025-11-18 16:40:46 +0530

    ORA_4bit_MISR_Design.v

--- ORA_4bit_MISR_Design.v
+++ ORA_4bit_MISR_Design.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
-module ORA_4bit_MISR (dataIn, clock, reset, dataOut);
-	input [1:0] dataIn;        // dataIn[0] = Sum, dataIn[1] = Cout
+module ORA_4bit_MISR (datain, clock, reset, dataout_misr);
+	input [1:0] datain;        // dataIn[0] = Sum, dataIn[1] = Cout
 	input clock, reset;
-	output reg [3:0] dataOut;
+	output reg [4:1] dataout_misr;
 	always @(posedge clock or negedge reset) begin
 		if(!reset) begin
-			dataOut <= 4'b0000; 
+			dataout_misr <= 4'b0000; 
 		end
 		else begin
-			dataOut[1] <= dataOut[4] ^ dataIn[0];              // feedback + dataIn[0]
-			dataOut[2] <= dataOut[3] ^ dataOut[1] ^ dataIn[1]; // feedback + dataIn[1]
-			dataOut[3] <= dataOut[2];                         // shift
-			dataOut[4] <= dataOut[3];                         // shift
+			dataout_misr[4] <= dataout_misr[3];
+			dataout_misr[3] <= dataout_misr[2];
+			dataout_misr[2] <= dataout_misr[4] ^ dataout_misr[1] ^ datain[1];  // feedback + dataIn[1]
+			dataout_misr[1] <= dataout_misr[4] ^ datain[0];                    // feedback + dataIn[0]
 		end
 	end
 endmodule
