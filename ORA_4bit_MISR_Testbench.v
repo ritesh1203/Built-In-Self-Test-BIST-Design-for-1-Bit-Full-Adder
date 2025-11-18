84454cf2b7fd1deb3b3d46d16de1edbfebe1e3e0
Ritesh Kumar <rk1516924@gmail.com>
2025-11-18 16:41:31 +0530

    ORA_4bit_MISR_Testbench.v

--- ORA_4bit_MISR_Testbench.v
+++ ORA_4bit_MISR_Testbench.v
@@ -1,26 +1,28 @@
 `timescale 1ns / 1ps
 module ORA_4bit_MISR_tb;
-    reg [1:0] dataIn;
+    reg [1:0] datain;
     reg clock, reset;
-    ORA_4bit_MISR  DUT (.dataIn(dataIn), .clock(clock), .reset(reset), .dataOut(dataOut));    // Instantiate the DUT (Device Under Test)
+    wire [4:1] dataout_misr;
+    ORA_4bit_MISR  DUT (.datain(datain), .clock(clock), .reset(reset), .dataout_misr(dataout_misr));    // Instantiate the DUT (Device Under Test)
     initial begin
-        clock = 1'b0;
-        reset  = 1'b0;
-        always #5 clock = ~ clock;
-    	$dumpfile ("ORA_4bit_MISR.vcd");
-    	$dumpvars (0, ORA_4bit_MISR_tb);
-    	$monitor ($time, "dataIn = %b, reset = %b, clock = %b, dataOut = %b", dataIn, reset, clock, dataOut);
-        dataIn = 2'b00;       // Initialize inputs (Sum=0, Cout=0)
-        #10 reset = 1'b1;    // Hold reset a bit longer, then release
+    	reset = 1'b0;
+    	#10 reset = 1'b1;    // Hold reset a bit longer, then release
+    	clock = 1'b0;
+        forever #5 clock = ~ clock;
+    end
+    initial begin
+    	$dumpfile("ORA_4bit_MISR.vcd");
+    	$dumpvars(0, ORA_4bit_MISR_tb);
+    	$monitor($time, "datain = %b, reset = %b, clock = %b, dataout_misr = %b", datain, reset, clock, dataout_misr);
+        datain = 2'b00;       // Initialize inputs (Sum=0, Cout=0)
         // Apply test patterns (Sum,Cout)
-        #10 dataIn = 2'b10;   // Sum=0, Cout=1
-        #10 dataIn = 2'b01;   // Sum=1, Cout=0
-        #10 dataIn = 2'b01;   // Sum=1, Cout=0
-        #10 dataIn = 2'b11;   // Sum=1, Cout=1
-        #10 dataIn = 2'b01;   // Sum=1, Cout=0
-        #10 dataIn = 2'b10;   // Sum=1, Cout=0
-        #10 dataIn = 2'b10;   // Sum=0, Cout=1
-        #200 $finish;
+        #10 datain = 2'b01;   // Cout=0, Sum=1 
+        #10 datain = 2'b10;   // Cout=1, Sum=0 
+        #10 datain = 2'b11;   // Cout=1, Sum=1 
+        #10 datain = 2'b10;   // Cout=1, Sum=0 
+        #10 datain = 2'b10;   // Cout=1, Sum=0 
+        #10 datain = 2'b01;   // Cout=0, Sum=1 
+        #10 datain = 2'b01;   // Cout=0, Sum=1 
+        #100 $finish;
     end
 endmodule
-
